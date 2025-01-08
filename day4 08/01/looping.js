f4a8f64405b81b81b5786ae20c2612798ceeacf4
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2025-01-08 13:19:49 +0530

    new files added

---
+++ day4 08/01/looping.js
@@ -0,0 +1,50 @@
+// for(let i = 0; i <= 10; i++) {
+//     console.log("JavaScrtipt");
+// }
+
+
+// ODD OR EVEN
+// for(let i = 1; i<=100; i++){
+//         if (i % 2 == 0){
+//             console.log(`${i}-even`)
+//         } else{
+//             console.log(`${i}-odd`)
+//         }  
+// }
+
+// for(let i = 1; i<=100; i++){
+//     console.log(`${i} - ${i % 2 == 0 ? 'even' : 'odd'}`)
+// }
+
+//FACTORIAL
+
+// let factorial = 1;
+// for(let i = 1; i <=5; i++){
+//     factorial *= i;
+//     console.log(factorial);
+// }
+
+// let mul = 1
+// let num = Number(prompt("Enter a number"))
+// for(let i = 1; i <=num; i++){
+//     mul *= i
+//     console.log(mul)
+// }
+
+//Assignment
+//To find the factors of a number
+// 12- 2, 4, 3,6
+
+
+
+// WHILE LOOP
+
+
+
+
+//do-while loop
+let i = 1
+do{
+    i++
+    console.log("Hello JavaScript")
+}while(i>=5);
