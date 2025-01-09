f6dd210f3470de6f3a6784e10e4b3e47ef2b6c57
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2025-01-09 10:21:25 +0530

    new files

---
+++ functions.js
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+//--------------------------------------------
+// without parameters
+
+// function demo(){
+//     console.log("object");
+// }
+// demo(); 
+
+//--------------------------------------------
+// function with parameters and arguments
+
+// let a = 10;  // a=parameter, 10=arguments
+// 5,6
+// 8,9
+// 3,5
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(5,6)
+// add(8,9)
+// add(3,5)
+
+//--------------------------------------------------------
+// named function
+
+// function test(){
+//     console.log("named function is executing...");
+// }
+// test()
+
+
+//----------------------------------------------------------
+// anonymus function
+// function (){
+
+// }
+// ()
+
+//--------------------------------------------------------
+// functions expressions
+// let a = 10
+// let x = function(){
+//     console.log("anonymus is executing with variable")
+// }
+// // console.log(x)
+// x()
+
+//----------------------------------------
+//IIFE
+// (
+//     function(){
+//         console.log("iife")
+//     }
+// )();
+
+//-----------------------------------------------
+// arrow function
+// function demo(a){
+//     console.log("hello")
+// }
+// demo()
+
+
+// let x = a=> console.log(a);
+// x(4)
+
+
+//--------------------------------------------
+// implicit returns and explicit return
+// function test1(a,b){
+//     return a+b
+// }
+// console.log(test1(5,5))
+
+// let x = (a,b) => {return a*b}
+// console.log(x(5,5))
+
+// let y = (a,b) => a+b
+// console.log(y(100,200))
