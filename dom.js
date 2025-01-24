5d25aae06394aed6b66d7982cda7383e434ab9b5
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2025-01-24 11:42:39 +0530

    new file added

---
+++ dom.js
@@ -0,0 +1,37 @@
+// let element = document.getElementById("demo");
+
+// console.log(element);
+
+// let test = document.getElementsByClassName("test"); 
+// // test.innerText = "<h1>Header</h1>";
+// console.log(test);
+
+
+let ele = document.getElementsByClassName("test");
+ele[0].style.backgroundColor = "dodgerblue";
+
+//let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele))
+
+
+
+// let x = [...ele]
+// comsole.log(x, Array.isArray(x));
+// x.map(element => {
+
+//     element.style.backgroundColor = "tomato";
+//     element.style.color = "white";
+//     element.style.textTranform = "uppercase";
+// })
+
+//*
+let ele = document.querySelector("#demo")
+console.log(ele);
+let ele = document.querySelector(".test")
+console.log(ele);
+
+
+// let ele = document.querySelectorAll(".test")
+// console.log(ele);
+
