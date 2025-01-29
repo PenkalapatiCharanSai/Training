a51cdec6914d4d85360571198e59e603f55367b2
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2025-01-29 11:40:28 +0530

    newfiles

---
+++ emp.js
@@ -0,0 +1,58 @@
+const employees =[
+    {
+        id: 1,
+        name: "Charan Sai",
+        company: "TCS",
+        salary: 50000,
+        address:{
+            city:"Hyderabad",
+            area:"Kondapur",
+        }
+        
+    },
+    {
+        id: 2,
+        name: "Sai Vasanth",
+        company: "Tech Mahindra",
+        salary: 60000,
+        address:{
+            city:"Hyderabad",
+            area:"Kukatpally",
+        }
+        
+    },
+    {
+        id: 3,
+        name: "Praharsha",
+        company: "Infosys",
+        salary: 40000,
+        address:{
+            city:"Benagluru",
+            area:"",
+        }
+        
+    },
+    {
+        id: 4,
+        name: "Sanyu Paul",
+        company: "Google",
+        salary: 80000,
+        address:{
+            city:"Hyderabad",
+            area:"Kukakpally",
+        }
+        
+    },
+    {
+        id: 5,
+        name: "Rajiu Ummar",
+        company: "Amazon",
+        salary: 60000,
+        address:{
+            city:"Hyderabad",
+            area:"Balnagar",
+        }
+        
+    }
+];
+console.log(employees);
