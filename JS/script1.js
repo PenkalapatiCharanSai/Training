613ad248d6d65cd6bcbb77871f2919eeab544c01
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2025-01-30 11:45:52 +0530

    newfiles

---
+++ JS/script1.js
@@ -0,0 +1,18 @@
+//^ Named Export
+// let msg = "hello, good morining"
+// function greet(){
+//     return msg+ " I am coding"
+// }
+
+
+// export {msg, greet}
+
+//^ Default export
+let msg = "hello, good morining"
+function greet(){
+    return msg+ " I am coding"
+}
+
+//let marks =90;
+
+export default greet; marks;
---
+++ JS/script2.js
@@ -0,0 +1,11 @@
+//^ Named export
+//import {msg , greet} from "./script1.js";
+// import  {greet}  from "./script1.js";
+
+// let abc = greet();
+// console.log(abc)
+// console.log(greet())
+
+//^Default export
+// import mru from "./script1.js";
+// console.log(mru())
