f82b20e76fc4d19d66f37111d0e23383ebbee5bd
”PenakalapatiCharanSai <pcharan87746@gmail.com>
2025-01-31 11:41:39 +0530

    newfiles

--- JS/script1.js
+++ JS/script1.js
@@ -7,12 +7,39 @@
 
 // export {msg, greet}
 
+
+// let x = 10;
+// export default x
+// let userDetail={
+//     name:"charan",
+//     profession:"student",
+// }
+// export default userDetail;
+let x = 10;
+const user = "charan";
+const users = ["charan", "vasanth", "sanyupaul", "rajiummar"];
+const obj = {
+    name: "charan"
+    // company: "xyz"
+};
+
+export { x, user, users, obj };
+
+
+
+
+
+
+
+
+
+
 //^ Default export
-let msg = "hello, good morining"
-function greet(){
-    return msg+ " I am coding"
-}
+// let msg = "hello, good morining"
+// function greet(){
+//     return msg+ " I am coding"
+// }
 
 //let marks =90;
 
-export default greet; marks;
+// export default greet; marks;
--- JS/script2.js
+++ JS/script2.js
@@ -1,4 +1,4 @@
-//^ Named export
+//^ Named import
 //import {msg , greet} from "./script1.js";
 // import  {greet}  from "./script1.js";
 
@@ -6,6 +6,39 @@
 // console.log(abc)
 // console.log(greet())
 
-//^Default export
+
+import { x, user, users, obj } from "./script1";
+
+console.log(x);
+console.log(user);
+
+console.log(users);
+users.map(user => {
+    console.log(user);
+});
+console.log(obj.name);
+
+// const college = "Your College Name";
+// console.log(college);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//^Default import
 // import mru from "./script1.js";
 // console.log(mru())
